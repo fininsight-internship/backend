41efdba29197209b187813bd6f3f230be4cf7833
woojin <tjdeo2gkfn@gmail.com>
2026-05-06 09:00:07 +0900

    feat: jd 분석 기본 API 구현

--- app/api/jd.py
+++ app/api/jd.py
@@ -1,1 +1,9 @@
 ## jd 분석 ##
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/jd/analyze")
+async def analyze_jd(company: str, job: str):
+    return {"company": company, "job": job}
+
--- app/main.py
+++ app/main.py
@@ -12,6 +12,6 @@ app.add_middleware(
 )
 
 app.include_router(company.router)
-# app.include_router(jd.router)
+app.include_router(jd.router)
 # app.include_router(resume.router)
 # app.include_router(interview.router)
